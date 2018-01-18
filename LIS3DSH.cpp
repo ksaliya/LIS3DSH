2a8409cb532e6986077951993905dfe65daf98f4
Saliya Kumara <saliyakumara@yahoo.com>
2018-01-18 12:00:52 -0500

    Initial upload

---
+++ LIS3DSH.cpp
@@ -0,0 +1,59 @@
+#include "LIS3DSH.h"
+
+LIS3DSH::LIS3DSH(){
+	SPI.begin();
+
+  // Set PORTB bit 1 as the CS
+  setbit(DDRB,1);
+  clearbit(PORTB,1);
+}
+
+void LIS3DSH::startup(){
+  // activate accelerometer at 100Hz
+  writeRegister(CTRL_REG4,0x67); // X, Y, Z enabled, ODR = 100 Hz
+  writeRegister(CTRL_REG3,0xC8); // DRY active high on INT1 pin
+}
+
+byte LIS3DSH::readRegister(byte Address){
+  byte result = 0;
+  clearbit(PORTB, PB1);
+  SPI.transfer(0x80 | Address);
+  result = SPI.transfer(0x00);
+  setbit(PORTB, PB1);
+  return(result);
+}
+
+void LIS3DSH::writeRegister(byte Address, byte Value){
+  clearbit(PORTB, PB1);
+  SPI.transfer(0x00 | Address);
+  SPI.transfer(Value);
+  setbit(PORTB, PB1);
+}
+
+int LIS3DSH::readAccelX(){
+  byte acc_x_L;
+  byte acc_x_H;
+  acc_x_H = readRegister(OUT_X_H);
+  acc_x_L = readRegister(OUT_X_L);
+  return (acc_x_H << 8) | acc_x_L;
+}
+
+int LIS3DSH::readAccelY(){
+  byte acc_y_L;
+  byte acc_y_H;
+  acc_y_H = readRegister(OUT_Y_H);
+  acc_y_L = readRegister(OUT_Y_L);
+  return (acc_y_H << 8) | acc_y_L;
+}
+
+int LIS3DSH::readAccelZ(){
+  byte acc_z_L;
+  byte acc_z_H;
+  acc_z_H = readRegister(OUT_Z_H);
+  acc_z_L = readRegister(OUT_Z_L);
+  return (acc_z_H << 8) | acc_z_L;
+}
+
+int LIS3DSH::readTemp(){
+  return readRegister(OUT_T);
+}
---
+++ LIS3DSH.h
@@ -0,0 +1,46 @@
+#ifndef LIS3DSH_H
+#define LIS3DSH_H
+
+#if (ARDUINO >= 100)
+  #include <Arduino.h>
+#else
+  #include <WProgram.h>
+#endif
+#include <SPI.h>
+
+#define setbit(port, bit) (port) |= (1 << (bit))
+#define clearbit(port, bit) (port) &= (0 << (bit))
+
+#define CTRL_REG1     0x21
+#define CTRL_REG2     0x22
+#define CTRL_REG3     0x23
+#define CTRL_REG4     0x20
+#define CTRL_REG5 		0x24
+#define CTRL_REG6 		0x25
+
+#define INFO1       0x0D
+#define INFO2       0x0E
+
+#define STATUS 			0x27
+#define WHO_AM_I  		0x0F
+#define OUT_X_L 		0x28
+#define OUT_X_H 		0x29
+#define OUT_Y_L 		0x2A
+#define OUT_Y_H 		0x2B
+#define OUT_Z_L 		0x2C
+#define OUT_Z_H 		0x2D
+#define OUT_T       0x0C
+
+class LIS3DSH{
+	public:
+		LIS3DSH();
+		void startup();
+		byte readRegister(byte Address);
+		void writeRegister(byte Address, byte Value);
+		int readAccelX();
+		int readAccelY();
+		int readAccelZ();
+    int readTemp();
+};
+
+#endif
